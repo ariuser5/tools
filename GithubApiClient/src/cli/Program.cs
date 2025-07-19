1a256e0923880f2e76c5bbd7337673ac78724e5c
Darius Emanuel Ciuverca <dariusciuverca@gmail.com>
2025-07-19 19:13:14 +0300

    Add capability to fetch user data

---
+++ GithubApiClient/src/cli/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http.Headers;
+
+using var client = new HttpClient();
+client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("DotNetApp", "1.0"));
+
+Console.Write("Enter GitHub username: ");
+var username = Console.ReadLine();
+var url = $"https://api.github.com/users/{username}";
+var response = await client.GetAsync(url);
+if (response.IsSuccessStatusCode)
+{
+    var content = await response.Content.ReadAsStringAsync();
+    Console.WriteLine($"GitHub User Profile for {username}:");
+    Console.WriteLine(content);
+}
+else
+{
+    Console.WriteLine($"Error: {response.StatusCode}");
+    var error = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(error);
+}
