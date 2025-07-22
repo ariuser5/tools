a1e5988c2219c8e340e576a09b87bcf7ad159659
Darius Emanuel Ciuverca <dariusciuverca@gmail.com>
2025-07-22 16:31:25 +0300

    Add initial implementation of Authenticator class and project file for GCP-Auth

---
+++ GCP-Auth/src/CredentialsProvider.cs
@@ -0,0 +1,29 @@
+﻿namespace DCiuve.Tools.Gcp.Auth;
+
+public class Authenticator
+{
+	public static async Task<UserCredential> Authorize(string secretJson)
+	{
+		var bytes = Encoding.UTF8.GetBytes(secretJson);
+		using var stream = new MemoryStream(bytes);
+		return await Authorize(stream);
+	}
+	
+	public static async Task<UserCredential> Authorize(Stream secretStream)
+	{
+		var credentialsFilePath = Environment.GetEnvironmentVariable(credentialsEnvVar)
+			?? throw new InvalidOperationException($"{credentialsEnvVar} environment variable is not set.");
+
+		UserCredential credential;
+		using var stream = new FileStream(credentialsFilePath, FileMode.Open, FileAccess.Read);
+		string credPath = "token.json";
+		credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+			clientSecrets: GoogleClientSecrets.FromStream(stream).Secrets,
+			scopes: [GmailService.Scope.GmailReadonly],
+			user: "user",
+			taskCancellationToken: CancellationToken.None,
+			dataStore: new FileDataStore(credPath, true));
+
+		return credential;
+	}
+}
