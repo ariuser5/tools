f078c6498f7edffd6b825e65a99c41659c3cc73e
Darius Emanuel Ciuverca <dariusciuverca@gmail.com>
2025-08-26 16:48:34 +0300

    Improve verbosity help text for ILogVerbosityOptions

--- DCiuve-Shared/src/Logging/LogVerbisityOptions.cs
+++ DCiuve-Shared/src/Logging/LogVerbisityOptions.cs
@@ -7,6 +7,8 @@ public interface ILogVerbosityOptions
 	[Option('v', "verbosity",
 		Required = false,
 		Default = LogLevel.Info,
-		HelpText = "Set the verbosity level for logging. Default is Info.")]
+		SetName = "verbosity",
+		HelpText = "Set the verbosity level for logging. Default is Info." +
+			" Verbosity levels: 0=Quiet, 1=Error, 2=Warning, 3=Info, 4=Debug.")]
 	LogLevel Verbosity { get; set; }
 }
